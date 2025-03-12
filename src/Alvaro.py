913deea346883eafc78fc44f756cbc4de634f27c
Alvaro <mpalvaro2004@gmail.com>
2025-03-12 19:29:58 +0100

    hola

---
+++ src/Alvaro.py
@@ -0,0 +1,16 @@
+def buscarPalabra(objetivo, palabras):
+
+
+
+def imprimirListaInversa(lista):
+    
+
+
+def = 
+nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
+edades = {
+    "Mengano": 0,
+    "Fulano": 25,
+    "Zutano": 50,
+    "Perantano": 75
+}
